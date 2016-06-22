216c594694db31e0e2c0a9ab1c47da8b3cf6c8cc
Ron <guangking1987@163.com>
2016-06-22 18:40:14 +0800

    11

--- TDD/readtxt.py
+++ TDD/readtxt.py
@@ -1,7 +1,13 @@
 #coding:utf-8
 def fileWrites():
-    f= file('c:/text.txt','w+')
+    f= file('f:/text.txt','w+')
     li=["selenium webdriver\n",'自动化测试\n']
     f.writelines(li)
     f.close()
-
+def readFile():
+     f= file('f:/text.txt','r')
+     print f.read()
+     f.close()
+if __name__=='__main__':
+    fileWrites()
+    readFile()
--- TDD/test.py
+++ TDD/test.py
@@ -3,3 +3,5 @@ from selenium import webdriver
 import time as t
 from selenium.webdriver.support.select import Select
 from selenium.webdriver.common.action_chains import ActionChains
+
+
